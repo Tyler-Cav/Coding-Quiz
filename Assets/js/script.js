6259ffbe5de287520d307d26a4ad2e5dd32e2471
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2023-11-28 23:11:47 -0500

    Updated to have questions as objects within a list

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,54 +1,44 @@
-
-//Trying to create button/list elements that I can push into HTML for each question
-//Lines 4-8 are going to the changing variables for each question that pops up.
-let questionEL = document.getElementById("questionPrompts")
-let choiceOneEl = document.getElementById("optionOne");
-let choiceTwoEl = document.getElementById("optionTwo");
-let choiceThreeEl = document.getElementById("optionThree");
-let choiceFourEl = document.getElementById("optionFour");
-
-
 let timeLeftEl = document.getElementById("timer");
 let h2TimeUpEl = document.getElementById("timeUp");
 let secondsLeft = 20
 let secondsInterval = setInterval(function () {
-secondsLeft--;
-timeLeftEl.textContent = secondsLeft + " Seconds Remaining";
-if (secondsLeft == 0) {
-    clearInterval(secondsInterval);
-    timeLeftEl.textContent = "Times Up!";
-    h2TimeUpEl.textContent = "Times Up!";
+    secondsLeft--;
+    timeLeftEl.textContent = secondsLeft + " Seconds Remaining";
+    if (secondsLeft == 0) {
+        clearInterval(secondsInterval);
+        timeLeftEl.textContent = "Times Up!";
+        h2TimeUpEl.textContent = "Times Up!";
     }
-},200);
+}, 200);
 //line below used as comparison example for when user gets question wrong
 if (1 === 1) {
     secondsLeft = secondsLeft - 10
 }
 
-quizTime();
-
-function questionPrompt() {
-    questionEL.textContent = "WHAT DO YOU WANT FROM ME?";
-    choiceOneEl.textContent = "Option 1";
-    choiceTwoEl.textContent = "Option 2";
-    choiceThreeEl.textContent = "Option 3";
-    choiceFourEl.textContent = "Option 4";
-
-}
-
-
-
-questionPrompt();
-
-// function questionOne{
-
-// }
-
-// function quizTime() {
-
-//}
-
-
-// looking for a way to create the question section. Quiz game function, 
-//call the question function 5 times
-//display questions in order
+var questions = [
+    {
+        prompt: "What is the basic keyword to create a loop?",
+        options: ["for", "door", "floor", "core"],
+        answer: "for"
+    },
+    {
+        prompt: "What other scope besides global exists?",
+        options: ["secular", "singular", "neighbor", "local"],
+        answer: "local"
+    },
+    {
+        prompt: "What file extension makes HTML pretty?",
+        options: [".html", ".js", ".css", ".jpeg"],
+        answer: ".css"
+    },
+    {
+        prompt: "What event listener responds to a mouse press?",
+        options: ["click", "clock", "push", "poke"],
+        answer: "click"
+    },
+    {
+        prompt: "What is the keyword used in your terminal to communicate between your local computer and GitHub?",
+        options: ["gat", "git", "get", "got"],
+        answer: "git"
+    }
+] 
