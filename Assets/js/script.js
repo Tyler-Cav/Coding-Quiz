83d2abc0416d438aa3de6c835f55aba91f2a3133
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2023-11-28 22:41:24 -0500

    changing logic on how quiz is formatted

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,26 +1,44 @@
 
 //Trying to create button/list elements that I can push into HTML for each question
- choiceOneEl = document.body.children[1].children[0].children[1].children[0]
- choiceTwoEl = document.body.children[1].children[0].children[1].children[1]
- choiceThreeEl = document.body.children[1].children[0].children[1].children[2]
- choiceFourEl = document.body.children[1].children[0].children[1].children[3]
+//Lines 4-8 are going to the changing variables for each question that pops up.
+let questionEL = document.getElementById("questionPrompts")
+let choiceOneEl = document.getElementById("optionOne");
+let choiceTwoEl = document.getElementById("optionTwo");
+let choiceThreeEl = document.getElementById("optionThree");
+let choiceFourEl = document.getElementById("optionFour");
 
-function quizTime () {
-    let timeLeftEl = document.getElementById("timer");
-    let h2TimeUpEl = document.getElementById("timeUp");
-    secondsLeft = 20
-    let secondsInterval = setInterval(function () {
-    secondsLeft--;
-    timeLeftEl.textContent = secondsLeft + " Seconds Remaining";
-    if (secondsLeft == 0) {
-        clearInterval(secondsInterval);
-        timeLeftEl.textContent = "Times Up!";
-        h2TimeUpEl.textContent = "Times Up!";
-        //Prompt end screen with scores based on what answers they were given.
+
+let timeLeftEl = document.getElementById("timer");
+let h2TimeUpEl = document.getElementById("timeUp");
+let secondsLeft = 20
+let secondsInterval = setInterval(function () {
+secondsLeft--;
+timeLeftEl.textContent = secondsLeft + " Seconds Remaining";
+if (secondsLeft == 0) {
+    clearInterval(secondsInterval);
+    timeLeftEl.textContent = "Times Up!";
+    h2TimeUpEl.textContent = "Times Up!";
     }
-    },100);
+},200);
+//line below used as comparison example for when user gets question wrong
+if (1 === 1) {
+    secondsLeft = secondsLeft - 10
 }
+
 quizTime();
+
+function questionPrompt() {
+    questionEL.textContent = "WHAT DO YOU WANT FROM ME?";
+    choiceOneEl.textContent = "Option 1";
+    choiceTwoEl.textContent = "Option 2";
+    choiceThreeEl.textContent = "Option 3";
+    choiceFourEl.textContent = "Option 4";
+
+}
+
+
+
+questionPrompt();
 
 // function questionOne{
 
