2457cd97e92462688d1ba64d3fbfe10a19ba7d3d
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2023-11-29 00:15:04 -0500

    created timer function, created questions, created event listener for each button on question options

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,19 +1,43 @@
+
 let timeLeftEl = document.getElementById("timer");
 let h2TimeUpEl = document.getElementById("timeUp");
-let secondsLeft = 20
-let secondsInterval = setInterval(function () {
-    secondsLeft--;
-    timeLeftEl.textContent = secondsLeft + " Seconds Remaining";
-    if (secondsLeft == 0) {
-        clearInterval(secondsInterval);
-        timeLeftEl.textContent = "Times Up!";
-        h2TimeUpEl.textContent = "Times Up!";
-    }
-}, 200);
-//line below used as comparison example for when user gets question wrong
-if (1 === 1) {
-    secondsLeft = secondsLeft - 10
+let secondsLeft = 0
+
+var startUpButton = document.getElementById("startUp")
+startUpButton.addEventListener("click", function() {
+    secondsLeft = 90
+    startTimer()
+    
+})
+
+function startTimer () {
+    setInterval(function () {
+        secondsLeft--;
+        timeLeftEl.textContent = secondsLeft + " Seconds Remaining";
+        if (secondsLeft == 0) {
+            clearInterval(secondsInterval);
+            timeLeftEl.textContent = "Times Up!";
+            h2TimeUpEl.textContent = "Times Up!";
+        }
+    }, 1000);
 }
+
+
+var optionElements = document.querySelectorAll("#options li button")
+//create event listener. Make function to see what it's calling for
+optionElements.forEach(function(optionElement){
+    optionElement.addEventListener("click", function(event) {
+        console.log(event);
+        //when user clicks an option
+        //get the value of the button that has been clicked
+        //compare teh value that was clicked to the current question answer
+        //if both are the same, move onto next question
+        //else remove 10 seconds, move onto next question
+    })
+});
+
+
+var questionNumber = 1
 
 var questions = [
     {
