8135f5f46b7a3f3357d92452308a6b4e4206a0ed
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2023-11-29 19:49:37 -0500

    feature added that moves to the next question. End screen added after all questions answered.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -55,14 +55,14 @@ startUpButton.addEventListener("click", function() {
     fourthOption.textContent = testQuestion.options[3]
     fourthOption.value = testQuestion.options[3]
 }) 
-    //have evnt listener on buttons already
-    //current question tracker
 
 
+//check if answer was selected function
 function isCorrectAnswer (selectedOption, answer) {
     return selectedOption === answer
 }
 
+//Timer Function
 function startTimer () {
     var secondsRemaining = setInterval(function () {
         secondsLeft--;
@@ -75,20 +75,40 @@ function startTimer () {
     }, 1000);
 }
 
-
 var optionElements = document.querySelectorAll("#options li button")
 //create event listener. Make function to see what it's calling for
 optionElements.forEach(function(optionElement){
     optionElement.addEventListener("click", function(event) {
         console.log(event)
         console.log(isCorrectAnswer(event.target.value, questions[questionNumber-1].answer ))
+        questionNumber += 1
+        if (questionNumber < questions.length + 1) {
+            let testQuestion = questions[questionNumber-1]
+            let secondQuestionPrompt = document.getElementById("questionPrompt")
+            secondQuestionPrompt.textContent = testQuestion.prompt
+            let firstOption = document.getElementById("optionOne")
+            let secondOption = document.getElementById("optionTwo")
+            let thirdOption = document.getElementById("optionThree")
+            let fourthOption = document.getElementById("optionFour")
+            firstOption.textContent = testQuestion.options[0]
+            firstOption.value = testQuestion.options[0]
+            secondOption.textContent = testQuestion.options[1]
+            secondOption.value = testQuestion.options[1]
+            thirdOption.textContent = testQuestion.options[2]
+            thirdOption.value = testQuestion.options[2]
+            fourthOption.textContent = testQuestion.options[3]
+            fourthOption.value = testQuestion.options[3]
+        } else {
+            questionElement.classList.add("hide")
+            let endScreen = document.querySelector("#gameOver")
+            endScreen.classList.remove("hide")
+            timeLeftEl
+        }
+    })
+});
 
         //when user clicks an option
         //get the value of the button that has been clicked
         //compare teh value that was clicked to the current question answer
         //if both are the same, move onto next question
         //else remove 10 seconds, move onto next question
-    })
-});
-
-
